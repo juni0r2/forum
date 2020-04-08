6f6662940e5a83d5e42cf68fe96b539d5190baad
israel franca <israel.franca@azi.com.br>
2020-04-08 08:41:05 -0400

    listando dados com jpaRepository

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,20 +1,26 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.model.Curso;
-import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TopicosController {
 
+    @Autowired
+    private TopicoRepository topicoRepository;
+
     @GetMapping("/topicos")
-    public List<TopicoDto> lista(){
-        Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
-        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+    public List<TopicoDto> lista(String nomeCurso) {
+
+        if (nomeCurso == null) {
+            return TopicoDto.converter(topicoRepository.findAll());
+        } else {
+            return TopicoDto.converter(topicoRepository.carregarPorNomeCurso(nomeCurso));
+        }
     }
 }
