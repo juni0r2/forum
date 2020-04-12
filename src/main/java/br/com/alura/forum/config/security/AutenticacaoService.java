9557f6dab1773f1ec4d759ae6828ee8c2f501463
israel franca <israel.franca@azi.com.br>
2020-04-11 23:47:02 -0400

    autenticando a aplicacao

---
+++ src/main/java/br/com/alura/forum/config/security/AutenticacaoService.java
@@ -0,0 +1,30 @@
+package br.com.alura.forum.config.security;
+
+import br.com.alura.forum.model.Usuario;
+import br.com.alura.forum.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String user) throws UsernameNotFoundException {
+
+        Optional<Usuario> optional = this.usuarioRepository.findByEmail(user);
+
+        if (optional.isPresent()) {
+            return optional.get();
+        }
+        throw new UsernameNotFoundException("Nenhum user encontrado");
+
+    }
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$cNSSkY4J4.DZsdf5qM7NQOTumxIxEoi.y.zilPIKIt0PPlTWZKTRG');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
