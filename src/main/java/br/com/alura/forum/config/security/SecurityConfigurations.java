8ec8b5816a397fc43172271fec6fc4664102a102
israel franca <israel.franca@azi.com.br>
2020-04-14 09:29:08 -0400

    adicionando swagger ao projeto

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -57,9 +57,8 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     //Configuração de recursos estáticos (js, css, imagens, etc.)
     @Override
     public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
     }
 
-    public static void main(String[] args) {
-        System.out.println(new BCryptPasswordEncoder().encode("123456"));
-    }
 }
